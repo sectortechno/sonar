6c67dabbb731c7c69bfc52d2094ceb74397c1dae
Shubhashish Shukla <amar.sshukla@gmail.com>
2018-03-13 00:59:45 +0530

    Spring Boot app bootstrapped

---
+++ src/main/java/st/sonar/java/SonarJava/SonarJavaApplication.java
@@ -0,0 +1,12 @@
+package st.sonar.java.SonarJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarJavaApplication.class, args);
+	}
+}
